0bfbcf72d755f462f7d98191525522c4911c8a7c
TgkCapture <tawongakanyenda5@gmail.com>
2025-03-10 22:23:54 +0200

    feat: data structures

---
+++ src/models.rs
@@ -0,0 +1,11 @@
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+pub struct Stock {
+    pub symbol: String,
+    pub open_price: f64,
+    pub close_price: f64,
+    pub percent_change: f64,
+    pub volume: f64,
+    pub turnover: f64,
+}
