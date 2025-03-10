08891fa7067a5d1d383b62aa20d843ce4691fd7b
TgkCapture <tawongakanyenda5@gmail.com>
2025-03-10 23:07:25 +0200

    feat: using utils

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,24 @@ mod utils;
 
 use config::get_env_var;
 use scraper::fetch_stock_data;
-use utils::{log_info, save_to_json};
+use utils::{log_error, log_info, log_warning, save_to_json};
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting stock scraper...");
+    log_info("Starting Market Miner...");
 
     let url = get_env_var("STOCK_API_URL");
-    log::info!("Fetching data from: {}", url);
+    log_info(&format!("Fetching data from: {}", url));
 
     match fetch_stock_data(&url).await {
         Ok(stocks) => {
             log_info(&format!("Fetched {} stocks", stocks.len()));
             save_to_json(&stocks, "data/stocks.json");
         }
-        Err(e) => log::error!("Error fetching stock data: {}", e),
+        Err(e) => {
+            log_error(&format!("Error fetching stock data: {}", e));
+            log_warning("Check your internet connection or the website URL.");
+        }
     }
 }
