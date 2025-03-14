a3ab0b8e0680f1254121e905f8f15885a7b56dd1
TgkCapture <tawongakanyenda5@gmail.com>
2025-03-14 11:55:45 +0200

    Removed old files after making the project modular

--- src/config.rs
+++
@@ -1,7 +0,0 @@
-use dotenv::dotenv;
-use std::env;
-
-pub fn get_env_var(key: &str) -> String {
-    dotenv().ok(); 
-    env::var(key).unwrap_or_else(|_| panic!("{} must be set in the .env file", key))
-}
