4340f86ac1597011d1bbc5df45d757702378beec
TgkCapture <tawongakanyenda5@gmail.com>
2025-03-14 10:34:25 +0200

    feat: Defines the Stock struct.

---
+++ src/models/stock.rs
@@ -0,0 +1,13 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Stock {
+    pub id: i32,
+    pub symbol: String,
+    pub open_price: f64,
+    pub close_price: f64,
+    pub percent_change: f64,
+    pub volume: f64,
+    pub turnover: f64,
+    pub timestamp: String,
+}
