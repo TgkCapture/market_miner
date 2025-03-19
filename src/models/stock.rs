75cbeded422e18fae6783c8db69d5a338ff1c4f5
TgkCapture <tawongakanyenda5@gmail.com>
2025-03-19 13:00:11 +0200

    Fix: using time generated by db

--- src/models/stock.rs
+++ src/models/stock.rs
@@ -1,13 +1,15 @@
+// models/stock.rs
+use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Stock {
-    pub id: i32,
+    pub id: i32, 
     pub symbol: String,
     pub open_price: f64,
     pub close_price: f64,
     pub percent_change: f64,
     pub volume: f64,
     pub turnover: f64,
-    pub timestamp: String,
+    pub timestamp: DateTime<Utc>, 
 }
